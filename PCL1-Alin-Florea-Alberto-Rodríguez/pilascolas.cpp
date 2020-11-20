46a5ca91d392d38d0270c51650094e84a93b3b12
AlinFlorea <74411199+AlinFlorea@users.noreply.github.com>
2020-11-20 13:56:47 +0100

    pepes

---
+++ PCL1-Alin-Florea-Alberto-Rodríguez/pilascolas.cpp
@@ -0,0 +1,60 @@
+#include "pilascolas.h"
+
+using namespace std;
+
+Pila::~Pila()
+{
+    while(cima) desapilar();
+}
+
+void Pila::apilar(Auto v)
+{
+    pNodo nuevo;
+
+    nuevo = new Nodo(v, cima);
+
+    cima = nuevo;
+}
+
+Auto Pila::desapilar()
+{
+    pNodo nodo;
+    Auto v;
+    Auto vnull(" "," "," ");
+
+    if(!cima) return vnull;
+
+    nodo = cima;
+
+    cima = nodo -> siguiente;
+
+    v = nodo -> valor;
+
+    delete nodo;
+
+    return v;
+}
+
+Cola::~Cola()
+{
+    while(primero) desencolar();
+}
+
+void Cola:encolar(Auto v)
+{
+    pNodo nuevo;
+
+    nuevo = new Nodo(v);
+
+    if(ultimo) ultimo->siguiente = nuevo;
+
+    ultimo = nuevo;
+
+    if(!primero) primero = nuevo;
+}
+
+Auto Cola:desencolar(){
+    pNodo nodo:
+    Auto v;
+    Auto vnull;
+}
