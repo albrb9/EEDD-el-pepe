1cd78b9709d8fe3e1b9f0f60d8e9466e92da84b4
AlinFlorea <74411199+AlinFlorea@users.noreply.github.com>
2020-11-20 13:59:47 +0100

    eee

--- PCL1-Alin-Florea-Alberto-Rodríguez/pilascolas.h
+++ PCL1-Alin-Florea-Alberto-Rodríguez/pilascolas.h
@@ -1,1 +1,52 @@
+#ifndef PILASCOLAS_H
+#define PILASCOLAS_H
+
+#iclude <iostream>
+
+using namespace std;
+
+struct Auto {
+    string n_bastidor;
+    string modelo;
+    string color;
+    string estado;
+};
+
+class Nodo
+{
+    private:
+        Auto valor;
+        Nodo *siguiente*;
+        friend class Pila;
+        friend class Cola;
+    public:
+        Nodo(Auto V, Nodo *sig= Null)
+        {
+            valor = v;
+            siguiente = sig;
+        }
+
+};
+typedef Nodo * pNodo;
+
+class Pila
+{
+    private:
+        pNodo cima;
+    public:
+        Pila() : cime (NULL) {}
+        ~Pila();
+        void apilar (Auto v);
+        Auto despilar();
+};
+class Cola{
+    public:
+        Cola() : primero (NULL), ultimo (NULL){}
+        ~cola();
+        void encolar (Auto v)
+        Auto desencolar();
+    private:
+        pNodo primero, ultimo;
+};
+#endif // PILASCOLAS_H
 
