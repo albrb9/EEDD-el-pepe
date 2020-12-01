1581ec43f63c3ada29fd713537e1a22e2ea6d3e1
albrb <alberto.rodriguezb@edu.uah.es>
2020-12-01 11:00:06 +0100

    Funciona

--- PCL1-Alin-Florea-Alberto-Rodríguez/main.cpp
+++ PCL1-Alin-Florea-Alberto-Rodríguez/main.cpp
@@ -18,6 +18,7 @@ int main()
     Pila pila, pila1, pila2, pila_aux1, pila_aux2, pila_aux3;
     Auto coche1={"3547T12","Ateca","Blanco"};
     Auto coche2={"4213T12","Ibiza","Negro"};
+    Auto coche3={calcularSecuenciaAleatoria()};
     Auto cochenulo={" "," "," "," "};
     Auto cocheaux, cocheaux1, cocheaux2, cocheaux3, cocheaux4;
 
@@ -27,6 +28,7 @@ int main()
     int longcola1, longcola2, longcola3, longcola4, longcolamax;
     int longpila1, longpila2, longpila3, longpilamax;
     int pasosmax, tipo;
+    //Auto coche3= pila.apilar(calcularSecuenciaAleatoria);
 
 
     pasosmax = (N1/N2)+1;
@@ -40,6 +42,7 @@ int main()
     cout << "---------------------------------------------------------------------------------------------------------------------" << endl;
     cout << "Bastidor|" << " Modelo|" << "  Color|" << "Estado|"<<"Bastidor|" << " Modelo|" << "    Color|" << "Estado|"<<"Bastidor|" << " Modelo|" << "    Color|" << "Estado|"<<"Bastidor|" << " Modelo|" << "    Color|" << "Estado|"<< endl;
 //    cout << "Bastidor|" << << " Modelo|" << "  Color|" << "Estado|"<<"Bastidor|" << " Modelo|" << "    Color|" << "Estado|"<<"Bastidor|" << " Modelo|" << "    Color|" << "Estado|"<<"Bastidor|" << " Modelo|" << "    Color|" << "Estado|"<< endl;
+    cout << coche3.n_bastidor << coche3.color << coche3.modelo << coche3.estado << endl;
     cout << "---------------------------------------------------------------------------------------------------------------------" << endl;
     cout << "           Camion Bilbao:"   << "                   Camion Murcia:" << "                     Camion Salamanca:" << endl;
     cout << "---------------------------------------------------------------------------------------------------------------------" << endl;
