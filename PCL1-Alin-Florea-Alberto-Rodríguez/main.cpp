c956e2a8946df7812f14e35faedeea3a2f8bfad4
AlinFlorea <74411199+AlinFlorea@users.noreply.github.com>
2020-12-02 19:15:35 +0100

    popopp

--- PCL1-Alin-Florea-Alberto-Rodríguez/main.cpp
+++ PCL1-Alin-Florea-Alberto-Rodríguez/main.cpp
@@ -31,9 +31,9 @@ int main()
     for(int i = pasoact; pasoact <= pasosmax; pasoact++){
     cout << "Pasos de ejecucion: " << pasoact << "/" << pasosmax << endl;
     cout << "\n" << endl;
-    cout << "          Linea de produccion 1: |" << "          Linea de produccion 2:| " << "         Linea de produccion 3:|" << "           Linea de produccion 4:|" << endl;
-    cout << "--------------------------------------------------------------------------------------------------------------------------------------" << endl;
-    cout << "Bastidor| Modelo|  Color| Estado |Bastidor| Modelo|   Color|Estado| Bastidor| Modelo|  Color|Estado| Bastidor| Modelo|   Color|Estado|"<< endl;
+    cout << "        Linea de produccion 1: |" << "          Linea de produccion 2:| " << "         Linea de produccion 3:|" << "          Linea de produccion 4:|" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------------------------" << endl;
+    cout << "Bastidor| Modelo| Color| Estado|Bastidor| Modelo|   Color|Estado| Bastidor| Modelo|  Color|Estado| Bastidor| Modelo|  Color|Estado|"<< endl;
 
     for(int i = 0; i <= 10; i++){
         Auto coche3={calcularSecuenciaAleatoria()};
@@ -52,9 +52,9 @@ int main()
                 pila4.apilar(coche3);
            }
     }
-while (pila1.longPila() != 0 or pila2.longPila() != 0 or pila3.longPila() != 0 or pila4.longPila() != 0){
+        while (pila1.longPila() != 0 or pila2.longPila() != 0 or pila3.longPila() != 0 or pila4.longPila() != 0){
         cocheaux1 = pila1.desapilar();
-        cout << setw(2) << cocheaux1.n_bastidor << setw(8)<< cocheaux1.modelo << setw(8) << cocheaux1.color << setw(8)<< cocheaux1.estado << "|" << setw(8);
+        cout << setw(1) << cocheaux1.n_bastidor << setw(8)<< cocheaux1.modelo << setw(8) << cocheaux1.color << setw(8)<< cocheaux1.estado << "|" << setw(8);
         pila_aux1.apilar(cocheaux1);
         cocheaux2 = pila2.desapilar();
         cout << cocheaux2.n_bastidor << setw(8)<< cocheaux2.modelo << setw(8) << cocheaux2.color << setw(8)<< cocheaux2.estado << "|" << setw(8);
@@ -64,8 +64,20 @@ while (pila1.longPila() != 0 or pila2.longPila() != 0 or pila3.longPila() != 0 o
         pila_aux3.apilar(cocheaux3);
         cocheaux4 = pila4.desapilar();
         cout << cocheaux4.n_bastidor << setw(8)<< cocheaux4.modelo << setw(8) << cocheaux4.color << setw(8)<< cocheaux4.estado << "|" << setw(8);
-        pila_aux4.apilar(cocheaux1);
+        pila_aux4.apilar(cocheaux4);
         cout << endl;
+        }
+        while (pila_aux1.longPila() != 0 or pila_aux2.longPila() != 0 or pila_aux3.longPila() != 0 or pila_aux4.longPila() != 0){
+            cocheaux1 = pila_aux1.desapilar();
+            pila1.apilar(cocheaux1);
+            cocheaux2 = pila_aux2.desapilar();
+            pila2.apilar(cocheaux2);
+            cocheaux3 = pila_aux3.desapilar();
+            pila3.apilar(cocheaux3);
+            cocheaux4 = pila_aux4.desapilar();
+            pila4.apilar(cocheaux4);
+
+
     }
     cout << "--------------------------------------------------------------------------------------------------------------------------------------" << endl;
     cout << "               Camion Bilbao:"   << "                      Camion Murcia:" << "                        Camion Salamanca:" << endl;
@@ -78,6 +90,7 @@ while (pila1.longPila() != 0 or pila2.longPila() != 0 or pila3.longPila() != 0 o
 
    }
 }
+
 
         /*cola.encolar(coche1);
         cout << "Encolando: " << coche1.n_bastidor << endl;
